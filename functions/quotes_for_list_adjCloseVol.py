66c7df85f519bc926d80ede606aebed6ad2a98e5
DonaldPG <donpg1@yahoo.com>
2014-02-09 20:48:16 -0600

    clean up print statements and comments used in debugging

--- functions/quotes_for_list_adjCloseVol.py
+++ functions/quotes_for_list_adjCloseVol.py
@@ -1,6 +1,5 @@
 import numpy as np
 import datetime
-#from matplotlib.pylab import *
 
 import datetime
 from scipy import random
@@ -28,7 +27,6 @@ def get_Naz100List( verbose=True ):
     ###
     ### get symbol list from previous period
     ###
-    #symbol_directory = os.getcwd() + "\\symbols"
     symbol_directory = os.path.join( os.getcwd(), "symbols" )
 
     symbol_file = "Naz100_symbols.txt"
@@ -44,9 +42,6 @@ def get_Naz100List( verbose=True ):
     try:
         base_url = 'http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx'
         content = urllib.urlopen(base_url).read()
-    
-        #print "\n\n\n"
-        #print "content = ", content
     
     
         m = re.search('var table_body.*?>*(?s)(.*?)<.*?>.*?<', content).group(0).split("],[")
@@ -70,14 +65,6 @@ def get_Naz100List( verbose=True ):
             for i in range( len(symbolList) ) :
                 f.write( symbolList[i] + ";" + companyNamesList[i] + "\n" )
     
-        '''
-        print "len of symbolList = ",len(symbolList)
-        for i in range( len(m) ):
-            print symbolList[i], companyNamesList[i]
-        print "****************"
-        print old_symbolList
-        print "****************"
-        '''
     
         ###
         ### compare old list with new list and print changes, if any
@@ -142,14 +129,11 @@ def get_Naz100List( verbose=True ):
         if removedTickers != [] or addedTickers != []:
             
             # make copy of previous symbols list file
-            #symbol_directory = os.getcwd() + "\\symbols"
             symbol_directory = os.path.join( os.getcwd(), "symbols" )
             symbol_file = "Naz100_symbols.txt"
             archive_symbol_file = "Naz100_symbols__" + str(datetime.date.today()) + ".txt"
             symbols_file = os.path.join( symbol_directory, symbol_file )
             archive_symbols_file = os.path.join( symbol_directory, archive_symbol_file )
-            #import shutil
-            #shutil.copy2(symbols_file, archive_symbol_file)
     
             with open( archive_symbols_file, "w" ) as f:
                 for i in range( len(old_symbolList) ) :
@@ -192,7 +176,6 @@ def get_Naz100PlusETFsList( verbose=True ):
     ###
     ### get symbol list from previous period
     ###
-    #symbol_directory = os.getcwd() + "\\symbols"
     symbol_directory = os.path.join( os.getcwd(), "symbols" )
     symbol_file = "Naz100PlusETFs_symbols.txt"
     symbols_file = os.path.join( symbol_directory, symbol_file )
@@ -251,14 +234,11 @@ def get_Naz100PlusETFsList( verbose=True ):
 
     if removedTickers != [] or addedTickers != []:
         # make copy of previous symbols list file
-        #symbol_directory = os.getcwd() + "\\symbols"
         symbol_directory = os.path.join( os.getcwd(), "symbols" )
         symbol_file = "Naz100_symbols.txt"
         archive_symbol_file = "Naz100_symbols__" + str(datetime.date.today()) + ".txt"
         symbols_file = os.path.join( symbol_directory, symbol_file )
         archive_symbols_file = os.path.join( symbol_directory, archive_symbol_file )
-        #import shutil
-        #shutil.copy2(symbols_file, archive_symbol_file)
 
         with open( archive_symbols_file, "w" ) as f:
             for i in range( len(old_symbolList) ) :
@@ -291,13 +271,8 @@ def arrayFromQuotesForList(symbolsFile, beginDate, endDate):
 
     # clean up quotes for missing values and varying starting date
     x=quote.copyx()
-    #print "larry getlabel 0 = ", quote.getlabel(0) ### dpg diagnostic
-    #print "larry getlabel 1 = ", quote.getlabel(1) ### dpg diagnostic
-    #print "larry getlabel 2 = ", quote.getlabel(2) ### dpg diagnostic
     date = quote.getlabel(2)
     datearray = np.array(date)
-
-    #print " x check: ",x[:,0,:][isnan(x[:,0,:])].shape
 
     # Clean up input quotes
     #  - infill interior NaN values using nearest good values to linearly interpolate
@@ -306,7 +281,6 @@ def arrayFromQuotesForList(symbolsFile, beginDate, endDate):
         x[ii,0,:] = interpolate(x[ii,0,:])
         x[ii,0,:] = cleantobeginning(x[ii,0,:])
 
-    #return x[:,0,:], symbols, datearray
     return x[:,0,:], quote.getlabel(0), datearray
 
 def arrayFromQuotesForListWithVol(symbolsFile, beginDate, endDate):
@@ -328,13 +302,8 @@ def arrayFromQuotesForListWithVol(symbolsFile, beginDate, endDate):
 
     # clean up quotes for missing values and varying starting date
     x=quote.copyx()
-    #print "larry getlabel 0 = ", quote.getlabel(0) ### dpg diagnostic
-    #print "larry getlabel 1 = ", quote.getlabel(1) ### dpg diagnostic
-    #print "larry getlabel 2 = ", quote.getlabel(2) ### dpg diagnostic
     date = quote.getlabel(2)
     datearray = np.array(date)
-
-    #print " x check: ",x[:,0,:][isnan(x[:,0,:])].shape
 
     # Clean up input quotes
     #  - infill interior NaN values using nearest good values to linearly interpolate
@@ -343,74 +312,18 @@ def arrayFromQuotesForListWithVol(symbolsFile, beginDate, endDate):
         x[ii,0,:] = interpolate(x[ii,0,:])
         x[ii,0,:] = cleantobeginning(x[ii,0,:])
 
-    #return x[:,0,:], symbols, datearray
     return x[:,0,:], x[:,1,:], quote.getlabel(0), datearray
 
-'''
-def LastQuotesForSymbolList(symbolList):
-    from matplotlib.finance import quotes_historical_yahoo
-    """
-    read in quotes and process to 'clean' ndarray plus date array
-    - prices in array with dimensions [num stocks : num days ]
-    - process stock quotes to show closing prices adjusted for splits, dividends
-    - single ndarray with dates common to all stocks [num days]
-    - clean up stocks by:
-       - infilling empty values with linear interpolated value
-       - repeat first quote to beginning of series
-    """
-    year = datetime.datetime.now().year
-    month = datetime.datetime.now().month
-    day = datetime.datetime.now().day
-    date2 = (int(year), int(month), int(day))
-    date2 = datetime.date.today() + datetime.timedelta(0)
-    # get quotes for each symbol in list (adjusted close)
-    quotelist = []
-    for i in range(len(symbolList)):
-        ticker = symbolList[i]
-        #print "ticker = ", ticker,date2
-        if ticker == 'CASH':
-            quotelist.append(1.0)
-        else:
-            data = quotes_historical_yahoo(ticker, date2, date2, asobject ="None")
-            #print "data = ", data
-            quotelist.append(data[0][6])
-    #quote = downloadQuotes(symbolList,date1=(year,month,day),date2=(year,month,day),adjust=False,Verbose=True)
-
-    """
-    # clean up quotes for missing values and varying starting date
-    x=quote.copyx()
-
-    # Clean up input quotes
-    #  - infill interior NaN values using nearest good values to linearly interpolate
-    #  - copy first valid quote to from valid date to all earlier positions
-    quotelist = []
-    for ii in range(x.shape[0]):
-        quotelist.append(x[ii,0,-1])
-    """
-    #print "quotelist =",quotelist
-    #return quotelist
-    return quotelist
-'''
 
 
 def get_quote_google( symbol ):
     import urllib
     import re
-    #print " get_quote_google.....  symbol = ", symbol
     base_url = 'http://finance.google.com/finance?q='
     content = urllib.urlopen(base_url + symbol).read()
-    #ref_34649_l
-    #m = re.search('id="ref_34649_l".*?>(.*?)<', content)
-    #n = re.search('id="ref_694653_l".*?>(.*?)<', content)
     m = re.search('class="pr".*?>*(?s)(.*?)<.*?>.*?<', content).group(0).split(">")[-1].split("<")[0]
-    #print "content = ", content
-    #print "m : ",content
-    #print "m = ", m
-    #from time import sleep
-    #sleep(20)
     if m :
         quote = m
-        #print "quote for symbol ", symbol, " = ", m
     else:
         quote = 'no quote available for: ' + symbol
     return quote
@@ -448,8 +361,6 @@ def LastQuotesForList( symbols_list ):
 
     stocks = StockRetriever()
 
-    print "inside LastQuotesForList... location  0"
-
     # remove 'CASH' from symbols_list, if present. Keep track of position in list to re-insert
     cash_index = None
     try:
@@ -459,8 +370,6 @@ def LastQuotesForList( symbols_list ):
     except:
         pass
 
-    print "inside LastQuotesForList... location  1"
-
     attempt = 1
     NeedQuotes = True
     while NeedQuotes:
@@ -468,13 +377,11 @@ def LastQuotesForList( symbols_list ):
             a=stocks.get_current_info( symbols_list )
             print "inside LastQuotesForList 1, symbols_list = ", symbols_list
             print "inside LastQuotesForList 1, attempt = ", attempt
-            #print "inside LastQuotesForList 1, len(a) = ", len(a)
             # convert from strings to numbers and put in a list
             quotelist = []
             for i in range(len(a)):
                 singlequote = np.float((a[i]['LastTradePriceOnly']).encode('ascii','ignore'))
                 quotelist.append(singlequote)
-            #quotelist= np.array(quotelist).tolist()
             print symbols_list, quotelist
             NeedQuotes = False
         except:
@@ -485,7 +392,6 @@ def LastQuotesForList( symbols_list ):
 
     # re-insert CASH in original position and also add curent price of 1.0 to quotelist
     if cash_index != None:
-        #print "inside LastQuotesForList... re-inserting...", cash_index, symbols_list, quotelist
         if cash_index < len(symbols_list):
             symbols_list[cash_index:cash_index] = 'CASH'
             quotelist[cash_index:cash_index] = 1.0
