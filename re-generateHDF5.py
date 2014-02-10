93da3b4f69d544d7782e2851a96d9dfa70f3a75c
DonaldPG <donpg1@yahoo.com>
2014-02-09 18:17:25 -0600

    include png images for building web page

--- re-generateHDF5.py
+++ re-generateHDF5.py
@@ -50,7 +50,7 @@ print "hdf5filename = ",hdf5filename
 ## Make a plot showing all symbols in list
 ##
 
-firstdate=(2014,1,1)
+firstdate=(2000,1,1)
 #firstdate=(2003,1,1)
 import datetime
 today = datetime.datetime.now()
