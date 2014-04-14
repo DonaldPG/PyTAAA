390e2eeab71bb4ded27a5fb6cfe8f75b3ddce561
Donald Griffith <donpg1@yahoo.com>
2014-04-13 20:29:58 -0500

    clean up references to labeled arrays

--- re-generateHDF5.py
+++ re-generateHDF5.py
@@ -1,22 +1,14 @@
 
 import os
 import numpy as np
-
 import datetime
-
 import nose
-#import bottleneck as bn
-#import la
-#import h5py
-
 import pandas as pd
 from pandas.io.data import get_data_yahoo
 
 from scipy.stats import gmean
-#from la.external.matplotlib import quotes_historical_yahoo
 
 ## local imports
-#from functions.quotes_for_list_adjCloseVol import *
 from functions.quotes_for_list_adjClose import *
 from functions.TAfunctions import *
 
@@ -53,16 +45,8 @@ print "hdf5filename = ",hdf5filename
 ## Clean up quotes.
 ## Make a plot showing all symbols in list
 ##
-'''
-firstdate=(1900,1,1)
-import datetime
-today = datetime.datetime.now()
-lastdate = ( today.year, today.month, today.day )
-'''
 import datetime
 firstdate=datetime.date(1900,1,1)
-lastdate='2011-11-30'
-lastdate='2013-6-1'
 today = datetime.date.today()
 lastdate = today
 
@@ -80,9 +64,6 @@ print "... first and last datearray are: ", datearray[0], datearray[-1]
 print "... first and last dates are: ", dates[0], dates[-1]
 
 # create pandas dataframe and write to hdf
-quotes_df = pd.DataFrame( adjClose.swapaxes(0,1), index=dates, columns=symbols)
+quotes_df = pd.DataFrame( adjClose.swapaxes(0,1), index=datearray, columns=symbols)
 quotes_df.to_hdf( hdf5filename, listname, mode='a',format='table',append=False,complevel=5,complib='blosc')
 
-'''io = la.IO(hdf5filename)
-io[listname] = quoteupdate
-'''
