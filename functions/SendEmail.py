4d7f62ac12b5b89b270fe4c6e93b8db110bca8ef
Donald Griffith <donpg1@yahoo.com>
2016-01-29 14:27:32 -0600

    add text message capability
    added text messaging when stock holding performance is outside narrow
    ragne

--- functions/SendEmail.py
+++ functions/SendEmail.py
@@ -17,7 +17,6 @@ Subject: """+message_subject+"""
 <b>"""+boldtext+"""</b>
 <p> </p>
 <p>"""+regulartext+"""</p>
-
 """
 
     # The actual mail send
@@ -35,4 +34,34 @@ Subject: """+message_subject+"""
         print "Error: unable to send email"
         print ""
 
+def SendTextMessage( username, emailpassword, phoneEmail, fromperson, message ) :
+    import smtplib
+    import datetime
+    # to, from, subject
+    fromaddr = fromperson
+    toaddrs  = phoneEmail
+    #message_from = "From: <"+fromaddr+">"
+    #message_to   = "To: <"+toaddrs+">"
+    message_subject = 'pyTAAA'
 
+    # The actual mail send
+    try:
+        print "\n ... inside SendTextMessage ..."
+        #print " ... username = ", username
+        #print " ... emailpassword = ", emailpassword
+        #print " ... message_from = ", message_from
+        #print " ... message_to = ", message_to
+        print " ... fromaddr = ", fromaddr
+        print " ... toaddr = ", phoneEmail
+        print " ... message_subject = ", message_subject
+        print " ... message = ", message, "\n\n"
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username,emailpassword)
+        server.sendmail(fromaddr, phoneEmail, message)
+        server.quit()
+        print "Successfully sent text message via email at ", datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+        print ""
+    except :
+        print "Error: unable to send text using email"
+        print ""
